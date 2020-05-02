2f3821ad373321e61ca7c9700421454d05cf9e3b
chio_kkk <anananfare@gmail.com>
2020-05-02 12:50:10 +0900

    script.jsをリファクタリング

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
-var div = document.getElementById('main');
-var btn = div.getElementsByClassName('button');
-console.dir(btn);
+//main buttonのリストを取得。
+var div_main = document.getElementById('main');
+var btn_main = div_main.getElementsByClassName('button');
+//result buttonのリストを取得。
+var div_result = document.getElementById('result');
+var btn_result = div_result.getElementsByClassName('button');
 
-var div2 = document.getElementById('result');
-var btn2 = div2.getElementsByClassName('button');
-console.dir(btn2);
 
 // ボタンの個数分ループ
-// 変数「i」に現在のループ回数が代入される
-for (var i = btn.length - 1; i >= 0; i--) {
-    btnAction(btn[i],i);
+for (var i = btn_main.length - 1; i >= 0; i--) {
+    btnAction(btn_main[i],i);
 }
 
 function btnAction(btnDOM, btnId) {
-    // 各ボタンをイベントリスナーに登録
     btnDOM.addEventListener("click", function () {
-        // activeクラスの追加と削除
-        // thisは、クリックされたオブジェクト
         this.classList.toggle('select');
-        btn2[btnId].classList.toggle('transparecy');
+        btn_result[btnId].classList.toggle('transparecy');
     })
 }
