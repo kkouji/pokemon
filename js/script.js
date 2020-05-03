f8d6e37d06938afdabb286915287aed86a9ddaf2
chio_kkk <anananfare@gmail.com>
2020-05-03 15:14:10 +0900

    タイプが１つ選ばれたときの実装完了。
    TODO:２個選ばれたときの実装。

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 
-var effect_table = [
+// タイプリスト。文字列を初期化するときなどに使う。
+const type_list = ["ノーマル","ほのお","みず","でんき","くさ","こおり","かくとう","どく","じめん","ひこう","エスパー","むし","いわ","ゴースト","ドラゴン","あく","はがね","フェアリ"];
+
+// 相性表。行が攻撃、列が防御、どの程度のダメージを与えられるかの２次元配列。
+const effect_table = [
 //   nor fir wat ele gla ice fig poi gro fly psy bug roc gho dra dar ste fair
     [1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1  ,1/2,0  ,1  ,1  ,1/2,1  ], // atack normal
     [1  ,1/2,1/2,1  ,2  ,2  ,1  ,1  ,1  ,1  ,1  ,2  ,1/2,1  ,1/2,1  ,2  ,1  ], // atack fire
@@ -21,14 +25,10 @@ var effect_table = [
     [1  ,1/2,1  ,1  ,1  ,1  ,2  ,1/2,1  ,1  ,1  ,1  ,1  ,1  ,2  ,2  ,1/2,1  ], // atack faily
 ];
 
-
 //main buttonのリストを取得。
-var div_main = document.getElementById('main');
-var btn_main = div_main.getElementsByClassName('button');
+var btn_main = document.getElementById('main').getElementsByClassName('button');
 //result buttonのリストを取得。
-var div_result = document.getElementById('result');
-var btn_result = div_result.getElementsByClassName('button');
-
+var btn_result = document.getElementById('result').getElementsByClassName('button');
 
 // ボタンの個数分ループ
 for (var i = 0 ; i < btn_main.length ; i++) {
@@ -47,15 +47,29 @@ function btnAction(btnDOM, defense_ID) {
             return false;
         }else if (select_num == 2) {
             //TODO:２種類選ばれたときの処理
-        }else if (select_num == 1) {
-            for (var j = 0 ; j < effect_table[defense_ID].length ; j++) {
-                if(effect_table[j][defense_ID] == 2){
-                    btn_result[j].insertAdjacentHTML('beforeend', '<b>○</b>');
-                    btn_result[j].classList.toggle('transparecy');//TODO:どうするかのポリシをきめる。
+        }else if (select_num == 1) {// 一つだけタイプ選ばれたときの処理。
+            for (var j = 0 ; j < effect_table[defense_ID].length ; j++) { // タイプを探索する。
+                btn_result[j].textContent = type_list[j];
+                btn_result[j].classList.remove('transparecy');
+                if(effect_table[j][defense_ID] == 2){//ダメージ2倍
+                    btn_result[j].insertAdjacentHTML('beforeend', ' <b>○</b>');
+                } else if(effect_table[j][defense_ID] == 1){//ダメージ1倍.このときだけ色を薄くする。
+                    btn_result[j].classList.add('transparecy');
+                } else if(effect_table[j][defense_ID] == 1/2){//ダメージ1/2倍
+                    btn_result[j].insertAdjacentHTML('beforeend', ' <b>△</b>');
+                } else if(effect_table[j][defense_ID] == 0){//ダメージ0倍
+                    btn_result[j].insertAdjacentHTML('beforeend', ' <b>×</b>');
                 }else{
-                    //TODO:どうするかのポリシをきめる。
+                    alert("Something wrong. Undefined damage data.");
                 }
             }
+        }else if (select_num == 0){ //タイプの選択がすべて解除されたとき
+            for (var j = 0 ; j < effect_table[defense_ID].length ; j++) { // タイプを探索する。
+                btn_result[j].textContent = type_list[j]; //デフォルトも文字列に戻す。
+                btn_result[j].classList.add('transparecy');//色を薄くする
+            }
+        }else{
+            alert("Something wrong. Programming Error");
         }
     })
 }
