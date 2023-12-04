c959f90b1e34a3ea62721561b60dfad2a7ec598d
chio_kkk <anananfare@gmail.com>
2023-12-04 20:47:01 +0900

    検索機能を実装完了

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var btn_reset = document.getElementById('main').getElementsByClassName('btn_06')
 var input_form = document.getElementById("name");
 
 // 選択ボタンの初期化
-for (var i = 0; i < btn_main.length; i++) {
+for (let i = 0; i < btn_main.length; i++) {
   btn_main[i].addEventListener("click", click_select )
 }
 //リセット表示の初期化
@@ -51,14 +51,14 @@ function click_select() {
 
 // mainボタンをデフォルトに戻す
 function resetMain() {
-  for (var j = 0; j < effect_table[0].length; j++) { // タイプを探索する。
+  for (let j = 0; j < effect_table[0].length; j++) { // タイプを探索する。
     btn_main[j].classList.remove('select');//selectを外す
     btn_main[j].classList.remove('transparecy');//色を濃くする
   }
 }
 // 結果ボタンをデフォルトに戻す
 function resetResult() {
-  for (var j = 0; j < effect_table[0].length; j++) { // タイプを探索する。
+  for (let j = 0; j < effect_table[0].length; j++) { // タイプを探索する。
     btn_result[j].textContent = type_list[j]; //デフォルトも文字列に戻す。
     btn_result[j].classList.add('transparecy');//色を薄くする
   }
@@ -71,19 +71,20 @@ function resetInput() {
   disp_pokemon_list.innerHTML = "";
 }
 
+//結果ボタンをアップデートする
 function updateResult() {
   //selectされているbtn_mainのindexリストを作成
   var select_list = document.getElementById('main').getElementsByClassName('select');
   var array_btn_main = btn_main;
   array_btn_main = [].slice.call(array_btn_main);
   let select_index = [];
-  for (var j = 0; j < select_list.length; j++) {
+  for (let j = 0; j < select_list.length; j++) {
     select_index.push(array_btn_main.indexOf(select_list[j]));
   }
 
   // btn_main操作
   if (select_list.length == 2) { // 2つselectされた場合選択されていないボタンを薄くする。
-    for (var i = 0; i < btn_main.length; i++) {
+    for (let i = 0; i < btn_main.length; i++) {
       if (select_index.includes(i)) {
         btn_main[i].classList.remove('transparecy');
       } else {
@@ -91,7 +92,7 @@ function updateResult() {
       }
     }
   } else if (select_list.length == 1) { // 1つselectされた場合、選択ボタンをすべて表示する。
-    for (var i = 0; i < btn_main.length; i++) {
+    for (let i = 0; i < btn_main.length; i++) {
       btn_main[i].classList.remove('transparecy');
     }
   }
@@ -100,10 +101,10 @@ function updateResult() {
   resetResult();
 
   //Result領域のアップデート
-  for (var j = 0; j < effect_table[0].length; j++) { // タイプを探索する。
+  for (let j = 0; j < effect_table[0].length; j++) { // タイプを探索する。
     //ダメージ計算
     var damage = 1;
-    for (var i = 0; i < select_list.length; i++) {
+    for (let i = 0; i < select_list.length; i++) {
       damage *= effect_table[j][select_index[i]];
     }
 
@@ -130,29 +131,34 @@ function updateResult() {
   }
 }
 
+//名前検索機能
 function search_pokemon(inputDOM) {
   inputDOM.addEventListener("input", function () {
-    var disp_pokemon_list = document.getElementById("pokemon_list");
-    var name = input_form.value;
-    var pokemon_name_list = [];
+    var disp_pokemon_list = document.getElementById("pokemon_list"); // 検索結果を出す領域
+    var name = input_form.value; //Inputフォーム
+    var pokemon_name_list = []; //全ポケモンの名前リスト
+
+    // ポケモンの全リストから名前リストを作る。
     for (let index = 0; index < pokemon_table.length; index++) {
       pokemon_name_list.push(pokemon_table[index][pokemon_table_name]);
     }
 
+    // Inputフォームに一致するポケモンの名前リストを作る
     var match_name_list = pokemon_name_list.filter(function (item) {
       return item.toLowerCase().includes(name.toLowerCase());
     });
 
+    // 結果表示を初期化
     disp_pokemon_list.innerHTML = "";
 
     //ポケモン選択ボタンを複数作成
     if (match_name_list.length != pokemon_name_list.length) {
-      match_name_list.forEach(function (pokomen_name) {
-        var pokemon_index = pokemon_name_list.indexOf(pokomen_name);
+      match_name_list.forEach(function (pokemon_name) {
+        var pokemon_index = pokemon_name_list.indexOf(pokemon_name);
         var pokemon_type1 = pokemon_table[pokemon_index][pokemon_table_type1]
         var pokemon_type2 = pokemon_table[pokemon_index][pokemon_table_type2]
 
-        disp_pokemon_list.insertAdjacentHTML('beforeend', '<div class="button">' + pokomen_name + "/" + pokemon_type1 + "/" + pokemon_type2 + "<br></div>");
+        disp_pokemon_list.insertAdjacentHTML('beforeend', '<div class="button">' + pokemon_name + "/" + pokemon_type1 + "/" + pokemon_type2 + "<br></div>");
 
       });
     }
@@ -160,14 +166,44 @@ function search_pokemon(inputDOM) {
     //作成したポケモン選択ボタンにアクションを追加
     //ポケモン選択ボタンのリストを取得。
     var btn_pokemon = document.getElementById('search').getElementsByClassName('button');
-    
-    for (var i = 0; i < btn_pokemon.length; i++) {
+
+    for (let i = 0; i < btn_pokemon.length; i++) {
       btn_pokemon[i].addEventListener("click", function (){
+        //　選択ボタン、結果ボタンを初期化
         resetMain();
         resetResult();
-        
-        //btn_mainの何番目を選択するのか
 
+        //選択さればボタンの情報を取得j
+        var pokemon_info = this.textContent.trim().split('/');
+        console.log(pokemon_info);
+        var pokemon_name  = pokemon_info[0];
+        var pokemon_type1 = pokemon_info[1];
+        var pokemon_type2 = pokemon_info[2];
+
+        //type_listからIndex番号を取得
+        var pokemon_type1_index = type_list.indexOf(pokemon_type1);
+        var pokemon_type2_index = type_list.indexOf(pokemon_type2);
+
+        //type listのIndex番号から選択ボタンをselect
+        btn_main[pokemon_type1_index].classList.toggle('select');
+        if(pokemon_type2_index > 0 ){
+          btn_main[pokemon_type2_index].classList.toggle('select');
+        }
+
+        //選択されているボタンから結果をアップデートする
+        var select_num = document.getElementById('main').getElementsByClassName('select').length;
+        if (select_num > 2) {
+          this.classList.toggle('select');
+          return false;
+        } else if (select_num == 2) { //２つタイプが選ばれたときの処理。
+          updateResult();
+        } else if (select_num == 1) {// 一つだけタイプが選ばれたときの処理。
+          updateResult();
+        } else if (select_num == 0) { //タイプの選択がすべて解除されたとき
+          resetResult();
+        } else {
+          alert("Something wrong. Programming Error");
+        }
       })
     }
   });
